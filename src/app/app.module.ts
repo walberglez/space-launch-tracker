5dc05777a0dcd076c767654b442b5cdb8644578d
Walber Gonzalez <walber.glez@gmail.com>
2019-09-30 21:26:06 -0400

    included files that were not committed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +17,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [],
