99467e5cd9d181df60b82555f4025dcf3970f4a5
walberglez <walber.glez@gmail.com>
2022-09-20 23:33:32 -0400

    added aws cdk spa deploy

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { SPADeploy } from 'cdk-spa-deploy';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+    new SPADeploy(this, 'spaDeploy')
+      .createBasicSite({
+        indexDoc: 'index.html',
+        websiteFolder: '../dist/space-launch-tracker'
+      });
   }
 }
